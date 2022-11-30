e3be89691f1b0b1819cbdf1c0bde784444a17d92
sakanal <sakanal9527@gmail.com>
2022-11-30 19:18:18 +0800

    更新本地图片

--- jsoup/src/main/java/pixiv/application.java
+++ jsoup/src/main/java/pixiv/application.java
@@ -3,6 +3,9 @@ package pixiv;
 import pixiv.URLConnection.PixivFollowing;
 import pixiv.URLConnection.PixivPicture;
 import pixiv.URLConnection.PixivUser;
+import pixiv.URLConnection.UpdataUser;
+
+import java.io.FileNotFoundException;
 
 public class application {
     private static final String proxyHost = "127.0.0.1";
@@ -18,7 +21,7 @@ public class application {
     //登录用户id
     private static final Integer myUserId=20225328;
     //画师id
-    private static final String userId="490219";
+    private static final String userId="8189060";
     //画作id
     private static final String pictureId="78468459";
     //下载地址
@@ -27,11 +30,11 @@ public class application {
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
 
-
+        updata();
 //        downloadByUser(userId);
 //        Integer[] userIdArray ={212801,2188232,1113943,2131660,1218472,1039353,8189060,490219,11729,480448};
 //        downloadByUser(userIdArray);
-        downloadByPictureId(userId,pictureId);
+//        downloadByPictureId(userId,pictureId);
 //        downloadByFollowing();
 
 
@@ -40,8 +43,20 @@ public class application {
         System.out.println("总共耗时"+((end-start)/1000)+"秒");
     }
 
+    public static void updata() throws Exception {
+        UpdataUser updataUser = null;
+        try {
+            updataUser = new UpdataUser(simpleDownloadDirName);
+        } catch (FileNotFoundException e) {
+            System.out.println("当前路径下没有画师图片文件夹");
+        }
+        if (updataUser!=null){
+            updataUser.download();
+        }
 
-    public static void downloadByPictureId(String userId,String pictureId) throws Exception{
+    }
+
+    public static void downloadByPictureId(String userId,String pictureId) {
         new PixivPicture(userId,pictureId,simpleDownloadDirName).download();
     }
     public static void downloadByUser(String userId) throws Exception {
