7927b6eaa76295c12069bc2282667ff5b5541f47
sakanal <sakanal9527@gmail.com>
2023-07-17 11:09:21 +0800

    通过Selenium简单实现猎聘爬虫，引入Pixiv的自动登录-需要在配置文件中配置账号密码

---
+++ selenium/src/main/java/com/sakanal/leipin/service/LiePinService.java
@@ -0,0 +1,10 @@
+package com.sakanal.leipin.service;
+
+import java.util.Map;
+
+public interface LiePinService {
+
+    void getLeiPinInfo(Map<String, String> params);
+
+    void relieveSign();
+}
