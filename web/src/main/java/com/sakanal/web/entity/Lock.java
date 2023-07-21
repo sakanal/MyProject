ee609c7b60f5ce0e02a9e5fdd4b5240ee03f527b
sakanal <sakanal9527@gmail.com>
2023-07-21 14:34:11 +0800

    更改数据库表名（lock-->my_lock）

--- web/src/main/java/com/sakanal/web/entity/Lock.java
+++ web/src/main/java/com/sakanal/web/entity/Lock.java
@@ -20,7 +20,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@TableName("lock")
+@TableName("my_lock")
 public class Lock implements Serializable {
 
     private static final long serialVersionUID = 1L;
