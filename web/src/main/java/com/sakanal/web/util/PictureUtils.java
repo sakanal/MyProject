44407061d4eaea171dc21809105d68621dc66a12
sakanal <sakanal9527@gmail.com>
2023-08-07 15:13:14 +0800

    添加图片复制功能，便于自动更新后进行查看

--- web/src/main/java/com/sakanal/web/util/PictureUtils.java
+++ web/src/main/java/com/sakanal/web/util/PictureUtils.java
@@ -72,6 +72,8 @@ public class PictureUtils {
         String[] split = src.split("\\.");
         String suffix = split[split.length - 1];
         String fileName = downloadDir + picture.getPictureId() + "_p" + picture.getPageCount()+ "_" + picture.getTitle()+"."+suffix;
+        String targetDir = downloadDir.replace(picture.getUserName() + "-" + picture.getUserId() + "\\", "temp\\");
+        String targetFileName = targetDir + picture.getPictureId() + "_p" + picture.getPageCount() + "_" + picture.getTitle() + "." + suffix;
         FileOutputStream outputStream = null;
         try {
             outputStream = new FileOutputStream(fileName);
@@ -79,7 +81,7 @@ public class PictureUtils {
             log.info("无法建立图片路径，请检查图片标题是否存在无效数据");
             e.printStackTrace();
         }
-        return download(inputStream, outputStream);
+        return download(inputStream, outputStream, fileName, targetDir, targetFileName);
     }
 
     private static boolean download(InputStream inputStream, FileOutputStream outputStream) {
@@ -96,6 +98,47 @@ public class PictureUtils {
             return false;
         }
         return true;
+    }
+
+
+    private static boolean download(InputStream inputStream, FileOutputStream outputStream, String fileName, String targetDir, String targetFileName) {
+        if (download(inputStream, outputStream)) {
+            // 下载成功，进行图片复制
+            File originFile = new File(fileName);
+            if (!originFile.exists()) {
+                log.error("下载成功的文件不存在");
+                // 下载的文件不存在表示该文件下载失败
+                return false;
+            }
+            File targetFileDir = createDir(targetDir);
+            if (targetFileDir == null) {
+                log.info("创建temp文件夹失败，无法记录更新数据");
+                // 下载的文件存在，所以认为下载成功，此时只是因为进行扩展功能时失败
+                return true;
+            }
+            try {
+                inputStream = new FileInputStream(originFile);
+                outputStream = new FileOutputStream(targetFileName);
+                int temp;
+                while ((temp = inputStream.read()) != -1) {
+                    outputStream.write(temp);
+                }
+                inputStream.close();
+                outputStream.close();
+                return true;
+            } catch (FileNotFoundException e) {
+                log.info("文件读取失败");
+                // 下载的文件存在，所以认为下载成功，此时只是因为进行扩展功能时失败
+                return true;
+            } catch (IOException e) {
+                log.info("文件复制失败--{}", fileName);
+                // 下载的文件存在，所以认为下载成功，此时只是因为进行扩展功能时失败
+                return true;
+            }
+        } else {
+            // 下载失败
+            return false;
+        }
     }
 
     private static String getSuffix(String src){
