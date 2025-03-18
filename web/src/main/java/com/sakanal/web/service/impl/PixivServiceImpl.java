d518af3fdba92b3bc4ae518c6cdc0250797d6ec8
sakanal <sakanal9527@gmail.com>
2025-03-18 17:20:57 +0800

    简化部分代码

--- web/src/main/java/com/sakanal/web/service/impl/PixivServiceImpl.java
+++ web/src/main/java/com/sakanal/web/service/impl/PixivServiceImpl.java
@@ -17,7 +17,6 @@ import com.sakanal.web.service.UserService;
 import com.sakanal.web.util.PictureUtils;
 import com.sakanal.web.util.PixivUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
